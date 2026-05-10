5949a2cd9c2d7ecb9fe0056aad90a0ee667d1690
Gamblerlucifer <gamblerlucifer@gmail.com>
2026-05-11 00:24:19 +0800

    feat: sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://wooriwin.com',
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
+  additionalPaths: async (config) => {
+    const fs = require('fs');
+    const path = require('path');
+    const postsDir = path.join(process.cwd(), 'data/posts');
+    const files = fs.readdirSync(postsDir);
+    return files.map((file) => ({
+      loc: `/blog/${file.replace('.json', '')}`,
+      changefreq: 'daily',
+      priority: 0.8,
+    }));
+  },
 }
